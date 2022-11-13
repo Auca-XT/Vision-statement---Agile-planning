607c28f73d9c9625ebf23a4f3126832f1f4bb162
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-11-13 20:14:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Agile vision Statement planning", '\n')
+#input vision statment parameters
+tgcust = input("Target customer: ")
+wneeds = input("Who needs: ")
+tname = input("The product name: ")
+isacat = input("Is a product category: ")
+vsthat = input("That product benefit, reason to buy: ")
+unlikec = input("Competitors: ")
+oproduct = input("Differentiation / Value proposition: ")
+print('\n')
+#output vision statement
+print("Agile vision Statement:", '\n')
+print("For", tgcust, "Who need", wneeds, "need a", tname, "to", isacat, "to get", vsthat, "unlike", unlikec, "we deliver:", '\n', oproduct)
